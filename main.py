3ea1f49299d104b770a178442bf85f6805156c09
Hafizh Ibnu Syam <aqrashu.asus@gmail.com>
2024-11-29 15:13:24 +0700

    init commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# matrix sample
+matrix = [[5,3,2,1],[1,2,10,1],[4,3,2,20],[2,1,2,1]]
+
+def main(matrix):
+    buckets = 0
+    index_matrix = 0
+    index_matrix_row = 0
+    for index, mat in enumerate(matrix):
+        index_row = 0
+        for j in mat:
+            if index_matrix_row > index_row:
+                index_row += 1
+                continue
+            buckets += j
+            if index_row < len(mat):
+                if index_row+1 == len(mat):
+                    break
+                if mat[index_row+1] <= matrix[index_matrix][index_row-1]:
+                    break
+            index_row += 1
+            index_matrix_row += 1
+        index_matrix += 1
+    return buckets
+
+buckets = main(matrix)
+
+print(f"{buckets} buckets")
